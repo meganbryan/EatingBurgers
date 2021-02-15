ae7eaeb053b84fde6893d5c773c1b2461602a80a
Megan Bryan <meganmbryan12@gmail.com>
2021-02-15 11:55:42 -0800

    Fix typo in file name on controller file

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const burger = require('../models/burger.js');
+const burger = require('../models/burgers.js');
 
 router.get('/', (req, res) => {
     burger.selectAll((data) => {
