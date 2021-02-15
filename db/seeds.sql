987044aaf5d47a318c1e5438efcfca5407bd62da
Megan Bryan <meganmbryan12@gmail.com>
2021-02-15 11:25:41 -0800

    Add schemas and run from the commend line

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Green Chili Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Smashburger');
