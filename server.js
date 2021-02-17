eaf56c5a7a2da75fe7c7c09218631bc7098773de
Megan Bryan <meganmbryan12@gmail.com>
2021-02-16 23:55:12 -0800

    Add const for handlebars express

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const PORT = process.env.PORT || 8000;
 const app = express();
+const expressHandlebars = require('express-handlebars');
 const routes = require('./controllers/burgers_controller.js');
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.use(routes);
 
