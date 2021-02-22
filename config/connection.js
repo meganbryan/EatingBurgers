3f6ad11c39a4e812b89e9638e7ea8014b9494198
Megan Bryan <meganmbryan12@gmail.com>
2021-02-21 17:20:57 -0800

    Change connection to check for JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 const mysql = require('mysql');
+let connection;
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'password',
-  database: 'burgers_db',
-});
+if (process.env.JAWSDB_WHITE) {
+  connection = mysql.createConnection(process.env.JAWSDB_WHITE);
+}
+else{
+  connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'password',
+    database: 'burgers_db',
+  });  
+}
 
 connection.connect((err) => {
   if (err) {
